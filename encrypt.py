d692b194d1c75fe3ec176938935a2dec9153636e
Jack O'Connor <oconnor663@gmail.com>
2015-12-15 17:18:05 -0500

    replace MAC groups with tag boxes

--- encrypt.py
+++ encrypt.py
@@ -2,7 +2,6 @@
 
 import base64
 from hashlib import sha512
-import hmac
 import io
 import json
 import os
@@ -61,89 +60,86 @@ def json_repr(obj):
     return json.dumps(_recurse_repr(obj), indent='  ')
 
 
+def counter(i):
+    'Turn the number into a 64-bit big-endian unsigned representation.'
+    return i.to_bytes(8, 'big')
+
+
 # All the important bits!
 # -----------------------
 
-def encrypt(sender_private, recipient_groups, message, chunk_size):
+def encrypt(sender_private, recipient_public_keys, message, chunk_size):
     sender_public = nacl.bindings.crypto_scalarmult_base(sender_private)
     ephemeral_private = os.urandom(32)
     ephemeral_public = nacl.bindings.crypto_scalarmult_base(ephemeral_private)
-    ephemeral_hash = sha512(ephemeral_public).digest()
-    # The NaCl nonce is 24 bytes. We room for a 4 byte counter.
-    nonce_start = ephemeral_hash[:20]
+    nonce_prefix_preimage = (
+        b"SaltPack\0" +
+        b"encryption nonce prefix\0" +
+        ephemeral_public)
+    nonce_prefix = sha512(nonce_prefix_preimage).digest()[:16]
     encryption_key = os.urandom(32)
-    mac_keys = []
-    recipient_num = 0
-    recipient_sets = []
 
-    # Populate all the recipient sets.
-    for group_num, group in enumerate(recipient_groups):
-        # The mac_key will be used to authenticate the payload packets for
-        # everyone in this recipient group.
-        mac_key = os.urandom(32)
-        mac_keys.append(mac_key)
-        for recipient in group:
-            # The sender box encrypts the real sender's public key. It's sent
-            # using ephemeral_private.
-            sender_box_counter_bytes = (2*recipient_num).to_bytes(4, 'big')
-            sender_box_nonce = nonce_start + sender_box_counter_bytes
-            sender_box = nacl.bindings.crypto_box(
-                message=sender_public,
-                nonce=sender_box_nonce,
-                pk=recipient,
-                sk=ephemeral_private)
+    recipient_tuples = []
+    recipient_beforenms = {}
+    for recipient_public in recipient_public_keys:
+        # The sender_box holds the sender's long-term public key. It's
+        # encrypted for each recipient with the ephemeral private key.
+        sender_box = nacl.bindings.crypto_box(
+            message=sender_public,
+            nonce=nonce_prefix + counter(0),
+            pk=recipient_public,
+            sk=ephemeral_private)
 
-            # The keys box encrypts the encryption_key and the mac_key. It's
-            # sent using sender_private.
-            keys = [
-                encryption_key,
-                group_num | (1 << 31),  # set the high bit for constant size
-                mac_key,
-            ]
-            keys_bytes = umsgpack.packb(keys)
-            keys_box_counter_bytes = (2*recipient_num + 1).to_bytes(4, 'big')
-            keys_box_nonce = nonce_start + keys_box_counter_bytes
-            keys_box = nacl.bindings.crypto_box(
-                message=keys_bytes,
-                nonce=keys_box_nonce,
-                pk=recipient,
-                sk=sender_private)
+        # The key_box holds the randomly generated encryption_key. It's
+        # encrypted for each recipient with the sender's long-term private key.
+        beforenm = nacl.bindings.crypto_box_beforenm(
+            pk=recipient_public,
+            sk=sender_private)
+        recipient_beforenms[recipient_public] = beforenm
+        key_box = nacl.bindings.crypto_box_afternm(
+            message=encryption_key,
+            nonce=nonce_prefix + counter(1),
+            k=beforenm)
 
-            # None is for the recipient public key, which is optional.
-            recipient_set = [None, sender_box, keys_box]
-            recipient_sets.append(recipient_set)
-            recipient_num += 1
+        # None is for the recipient public key, which is optional.
+        recipient_tuple = [None, sender_box, key_box]
+        recipient_tuples.append(recipient_tuple)
 
     header = [
         "sillybox",  # format name
         [1, 0],      # major and minor version
         0,           # mode (encryption, as opposed to signing/detached)
         ephemeral_public,
-        recipient_sets,
+        recipient_tuples,
     ]
     output = io.BytesIO()
     output.write(umsgpack.packb(header))
 
     # Write the chunks.
     for packetnum, chunk in enumerate(chunks_with_empty(message, chunk_size)):
-        nonce = packetnum.to_bytes(24, byteorder='big')
-        chunk_box = nacl.bindings.crypto_secretbox(
+        payload_nonce = nonce_prefix + counter(packetnum + 2)
+        payload_secretbox = nacl.bindings.crypto_secretbox(
             message=chunk,
-            nonce=nonce,
+            nonce=payload_nonce,
             key=encryption_key)
-        chunk_tag = chunk_box[:16]  # the Poly1305 authenticator
-        macs = []
-        for mac_key in mac_keys:
-            # Only take the first 32 bytes of HMAC-SHA512, as NaCl does.
-            chunk_tag_hmac = hmac.new(
-                key=mac_key,
-                msg=chunk_tag,  # TODO: mix in the packet number?
-                digestmod='sha512',
-                ).digest()[:32]
-            macs.append(chunk_tag_hmac)
+        payload_tag = payload_secretbox[:16]  # the Poly1305 authenticator
+        stripped_payload_secretbox = payload_secretbox[16:]
+        tag_boxes = []
+        for recipient_public in recipient_public_keys:
+            # Encrypt the payload_tag for each recipient. This isn't because we
+            # want to keep the tag secret, but because:
+            #   1) We want to authenticate the tag, to prove the sender
+            #      actually wrote it.
+            #   2) We want to force implementations to verify that.
+            beforenm = recipient_beforenms[recipient_public]
+            tag_box = nacl.bindings.crypto_box_afternm(
+                message=payload_tag,
+                nonce=payload_nonce,
+                k=beforenm)
+            tag_boxes.append(tag_box)
         packet = [
-            macs,
-            chunk_box,
+            tag_boxes,
+            stripped_payload_secretbox,
         ]
         output.write(umsgpack.packb(packet))
 
@@ -161,70 +157,62 @@ def decrypt(input, recipient_private):
         [major_version, minor_version],
         mode,
         ephemeral_public,
-        recipients,
+        recipient_tuples,
     ] = header
-    ephemeral_hash = sha512(ephemeral_public).digest()
-    ephemeral_shared = nacl.bindings.crypto_box_beforenm(
+    nonce_prefix_preimage = (
+        b"SaltPack\0" +
+        b"encryption nonce prefix\0" +
+        ephemeral_public)
+    nonce_prefix = sha512(nonce_prefix_preimage).digest()[:16]
+    ephemeral_beforenm = nacl.bindings.crypto_box_beforenm(
         pk=ephemeral_public,
         sk=recipient_private)
-    nonce_start = ephemeral_hash[:20]
 
     # Try decrypting each sender box, until we find the one that works.
-    for recipient_num, recipient_set in enumerate(recipients):
-        [_, sender_box, keys_box] = recipient_set
-        sender_box_counter_bytes = (2*recipient_num).to_bytes(4, 'big')
-        sender_box_nonce = nonce_start + sender_box_counter_bytes
+    for recipient_index, recipient_tuple in enumerate(recipient_tuples):
+        [_, sender_box, key_box] = recipient_tuple
         try:
             sender_public = nacl.bindings.crypto_box_open_afternm(
                 ciphertext=sender_box,
-                nonce=sender_box_nonce,
-                k=ephemeral_shared)
+                nonce=nonce_prefix + counter(0),
+                k=ephemeral_beforenm)
             break
         except CryptoError:
             continue
     else:
         raise RuntimeError('Failed to find matching recipient.')
 
-    # Decrypt the keys_box using sender_public.
-    keys_box_counter_bytes = (2*recipient_num + 1).to_bytes(4, 'big')
-    keys_box_nonce = nonce_start + keys_box_counter_bytes
-    keys_bytes = nacl.bindings.crypto_box_open(
-        ciphertext=keys_box,
-        nonce=keys_box_nonce,
+    # Decrypt the encryption_key using sender_public.
+    sender_beforenm = nacl.bindings.crypto_box_beforenm(
         pk=sender_public,
         sk=recipient_private)
-    keys = umsgpack.unpackb(keys_bytes)
-    print('Keys: ', end='')
-    print(json_repr(keys))
-    [encryption_key, masked_mac_group, mac_key] = keys
-    mac_group = masked_mac_group ^ (1 << 31)
-    print('MAC group:', mac_group)
+    encryption_key = nacl.bindings.crypto_box_open_afternm(
+        ciphertext=key_box,
+        nonce=nonce_prefix + counter(1),
+        k=sender_beforenm)
 
     # Decrypt each of the packets.
     output = io.BytesIO()
-    packetnum = 0
+    packetnum = 2
     while True:
+        payload_nonce = nonce_prefix + counter(packetnum)
         packet = umsgpack.unpack(stream)
         print('Packet: ', end='')
         print(json_repr(packet))
-        [macs, chunk_box] = packet
+        [tag_boxes, stripped_payload_secretbox] = packet
+        tag_box = tag_boxes[recipient_index]
 
-        # Check the MAC.
-        their_mac = macs[mac_group]
-        chunk_tag = chunk_box[:16]
-        chunk_tag_hmac = hmac.new(
-            key=mac_key,
-            msg=chunk_tag,
-            digestmod='sha512',
-            ).digest()[:32]
-        if not hmac.compare_digest(their_mac, chunk_tag_hmac):
-            raise RuntimeError("MAC mismatch!")
+        # Open the tag box.
+        payload_tag = nacl.bindings.crypto_box_open_afternm(
+            ciphertext=tag_box,
+            nonce=payload_nonce,
+            k=sender_beforenm)
 
-        # Unbox the chunk.
-        nonce = packetnum.to_bytes(24, byteorder='big')
+        # Prepend the tag and open the payload secretbox.
+        payload_secretbox = payload_tag + stripped_payload_secretbox
         chunk = nacl.bindings.crypto_secretbox_open(
-            ciphertext=chunk_box,
-            nonce=nonce,
+            ciphertext=payload_secretbox,
+            nonce=payload_nonce,
             key=encryption_key)
         output.write(chunk)
         print('Chunk:', chunk)
@@ -247,15 +235,18 @@ def main():
     else:
         encoded_message = message.encode('utf8')
     recipients_len = int(args.get('--recipients') or 1)
-    recipients_private = [os.urandom(32) for i in range(recipients_len)]
-    recipients_public = [nacl.bindings.crypto_scalarmult_base(r)
-                         for r in recipients_private]
-    groups = [[p] for p in recipients_public]
+    recipient_private_keys = [os.urandom(32) for i in range(recipients_len)]
+    recipient_public_keys = [nacl.bindings.crypto_scalarmult_base(r)
+                             for r in recipient_private_keys]
     chunk_size = int(args.get('--chunk') or 100)
-    output = encrypt(jack_private, groups, encoded_message, chunk_size)
+    output = encrypt(
+        jack_private,
+        recipient_public_keys,
+        encoded_message,
+        chunk_size)
     print(base64.b64encode(output).decode())
     print('-----------------------------------------')
-    decoded_message = decrypt(output, recipients_private[0])
+    decoded_message = decrypt(output, recipient_private_keys[0])
     print('message:', decoded_message)
 
 
