25980dd006dbeaccf37b51995c281ce0243487c5
Jack O'Connor <oconnor663@gmail.com>
2016-02-03 16:18:28 -0500

    require Python 3

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 import setuptools
+import sys
+
+if sys.version_info.major < 3:
+    print("ERROR: saltpack-python requires Python 3")
+    sys.exit(1)
 
 with open("saltpack/VERSION") as f:
     version = f.read().strip()
